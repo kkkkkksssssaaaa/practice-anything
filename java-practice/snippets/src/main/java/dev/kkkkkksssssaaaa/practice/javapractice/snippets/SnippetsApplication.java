b78bc56c05811a6e0307b6df18f9b69cd434e4a9
kkkkkksssssaaaa <kofc312@naver.com>
2024-01-04 22:51:11 +0900

    feat(java-practice); add new project

---
+++ java-practice/snippets/src/main/java/dev/kkkkkksssssaaaa/practice/javapractice/snippets/SnippetsApplication.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.practice.javapractice.snippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetsApplication.class, args);
+	}
+
+}
---
+++ java-practice/snippets/src/test/java/dev/kkkkkksssssaaaa/practice/javapractice/snippets/SnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.practice.javapractice.snippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
