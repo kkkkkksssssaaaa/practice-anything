3e6f91e9be3066d243d06ac9cc53171646212fd7
kkkkkksssssaaaa <kofc312@naver.com>
2024-06-09 22:13:03 +0900

    fix: add enable async annotation

--- templateserver/src/main/kotlin/dev/kkkkkksssssaaaa/practice/templateserver/TemplateServerApplication.kt
+++ templateserver/src/main/kotlin/dev/kkkkkksssssaaaa/practice/templateserver/TemplateServerApplication.kt
@@ -2,7 +2,9 @@ package dev.kkkkkksssssaaaa.practice.templateserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
 class TemplateServerApplication
 
