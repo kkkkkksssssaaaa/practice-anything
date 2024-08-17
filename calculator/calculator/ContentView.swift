753021f7b8da83c389fbd6849815505cc38aff5c
kkkkkksssssaaaa <kofc312@naver.com>
2024-08-17 23:31:42 +0900

    feat(calculator): draw buttons

--- calculator/calculator/ContentView.swift
+++ calculator/calculator/ContentView.swift
@@ -10,12 +10,143 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("7")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("8")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("9")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("X")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.orange)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+            }
+            HStack {
+                Text("4")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("5")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("6")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("-")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.orange)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+            }
+            HStack {
+                Text("1")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("2")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("3")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("+")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.orange)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+            }
+            HStack {
+                Text("0")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("0")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text(".")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.gray)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+                Text("=")
+                    .frame(
+                        width: 80,
+                        height: 80
+                    ).background(.orange)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .font(.system(size: 33))
+            }
         }
-        .padding()
     }
 }
 
