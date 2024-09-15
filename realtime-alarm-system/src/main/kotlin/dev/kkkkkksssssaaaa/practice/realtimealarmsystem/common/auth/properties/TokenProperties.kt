950470f3a8b1ce5eaa0c5ddfe12a6569331d0a5b
kkkkkksssssaaaa <kofc312@naver.com>
2024-09-16 00:07:21 +0900

    fix(realtime-alarm-system): fix environment property inject error

--- realtime-alarm-system/src/main/kotlin/dev/kkkkkksssssaaaa/practice/realtimealarmsystem/common/auth/properties/TokenProperties.kt
+++ realtime-alarm-system/src/main/kotlin/dev/kkkkkksssssaaaa/practice/realtimealarmsystem/common/auth/properties/TokenProperties.kt
@@ -1,15 +1,14 @@
 package dev.kkkkkksssssaaaa.practice.realtimealarmsystem.common.auth.properties
 
-import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-@ConfigurationProperties(prefix = "app.auth")
 data class TokenProperties(
-    val issuer: String,
-    val audience: String,
-    val accessTokenSecret: String,
-    val refreshTokenSecret: String,
-    val accessTokenExpiration: Long,
-    val refreshTokenExpiration: Long,
+    @Value("\${app.auth.issuer}") val issuer: String,
+    @Value("\${app.auth.audience}") val audience: String,
+    @Value("\${app.auth.access-token-secret}") val accessTokenSecret: String,
+    @Value("\${app.auth.refresh-token-secret}") val refreshTokenSecret: String,
+    @Value("\${app.auth.access-token-expiration}") val accessTokenExpiration: Long,
+    @Value("\${app.auth.refresh-token-expiration}") val refreshTokenExpiration: Long,
 )
