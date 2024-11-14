2f1308a0719bedbe4e4962b76a59fd8cc0b5e47c
kkkkkksssssaaaa <kofc312@naver.com>
2024-11-14 23:08:01 +0900

    feat(kafka-practice/checkout): initialize project

---
+++ kafka-practice/checkout/src/main/kotlin/dev/kkkkkksssssaaaa/practice/kafka/checkout/CheckoutApplication.kt
@@ -0,0 +1,11 @@
+package dev.kkkkkksssssaaaa.practice.kafka.checkout
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CheckoutApplication
+
+fun main(args: Array<String>) {
+    runApplication<CheckoutApplication>(*args)
+}
---
+++ realtime-alarm-system/src/main/kotlin/dev/kkkkkksssssaaaa/practice/realtimealarmsystem/domain/user/persistance/UserSubscribeAggregate.kt
@@ -0,0 +1,4 @@
+package dev.kkkkkksssssaaaa.practice.realtimealarmsystem.domain.user.persistance
+
+import com.querydsl.core.annotations.QueryProjection
+
