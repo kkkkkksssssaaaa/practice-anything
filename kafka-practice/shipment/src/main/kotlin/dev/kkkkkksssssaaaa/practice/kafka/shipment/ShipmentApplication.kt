6de3859ee07637243f0a798893c671517c146da3
kkkkkksssssaaaa <kofc312@naver.com>
2024-11-20 18:53:49 +0900

    feat(kafka-practice/shipment): initialize project

---
+++ kafka-practice/shipment/src/main/kotlin/dev/kkkkkksssssaaaa/practice/kafka/shipment/ShipmentApplication.kt
@@ -0,0 +1,11 @@
+package dev.kkkkkksssssaaaa.practice.kafka.shipment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShipmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShipmentApplication>(*args)
+}
---
+++ kafka-practice/shipment/src/test/kotlin/dev/kkkkkksssssaaaa/practice/kafka/shipment/ShipmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.practice.kafka.shipment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShipmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
