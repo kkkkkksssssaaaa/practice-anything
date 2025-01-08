5ed0b7137b0594a55ed085b9673149e24b4de612
kkkkkksssssaaaa <kofc312@naver.com>
2025-01-08 17:48:04 +0900

    fix(eda-tutorial/member): add ignored files

---
+++ eda-tutorial/eda-member/src/main/kotlin/dev/kkkkkksssssaaaa/practice/edamember/edamember/EdaMemberApplication.kt
@@ -0,0 +1,11 @@
+package dev.kkkkkksssssaaaa.practice.edamember.edamember
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdaMemberApplication
+
+fun main(args: Array<String>) {
+    runApplication<EdaMemberApplication>(*args)
+}
---
+++ eda-tutorial/eda-member/src/test/kotlin/dev/kkkkkksssssaaaa/practice/edamember/edamember/EdaMemberApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.practice.edamember.edamember
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdaMemberApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
