c888b76031beb87ae6979d6238ae8c553262828b
kkkkkksssssaaaa <kofc312@naver.com>
2025-01-08 19:58:46 +0900

    fix(eda-tutorial/payment): add ignored files

---
+++ eda-tutorial/eda-payment/src/main/kotlin/dev/kkkkkksssssaaaa/practice/edapayment/edapayment/EdaPaymentApplication.kt
@@ -0,0 +1,11 @@
+package dev.kkkkkksssssaaaa.practice.edapayment.edapayment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdaPaymentApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdaPaymentApplication>(*args)
+}
