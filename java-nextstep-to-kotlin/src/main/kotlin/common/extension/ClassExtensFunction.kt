f3a9cad7da01918b723aa90c3a8a6a970f1bc353
kkkkkksssssaaaa <kofc312@naver.com>
2025-08-18 23:53:53 +0900

    feat(nextstep-to-kotlin): refactor Class extension method to KClass extension method

--- java-nextstep-to-kotlin/src/main/kotlin/common/extension/ClassExtensFunction.kt
+++ java-nextstep-to-kotlin/src/main/kotlin/common/extension/ClassExtensFunction.kt
@@ -5,27 +5,35 @@ import kotlin.reflect.KClass
 
 private val log = KotlinLogging.logger {}
 
-fun <A : Annotation> Class<*>.isBean(annotation: KClass<A>): Boolean {
-    if (this.isAnnotationPresent(annotation.java)) return true
+fun <A : Annotation> KClass<*>.isBean(annotation: KClass<A>): Boolean {
+    if (this.annotations.any { it.annotationClass == annotation }) return true
 
-    return this.annotatedInterfaces.any {
-        (it.type as? Class<*>)?.isAnnotationPresent(annotation.java) ?: false
+    return this.annotations.any {
+        it.annotationClass == annotation
     }
 }
 
-fun <A : Annotation> Class<*>.getBeanName(annotation: KClass<A>): String {
-    if (this.isAnnotationPresent(annotation.java)) {
-        return this.simpleName
+fun <A : Annotation> KClass<*>.getBeanName(annotation: KClass<A>): String {
+    if (this.annotations.any { it.annotationClass == annotation }) {
+        return this.simpleName!!
     }
 
-    val targetInterface = this.annotatedInterfaces.find { annotatedType ->
-        (annotatedType.type as? Class<*>)?.isAnnotationPresent(annotation.java) == true
-    }
+    val targetInterface = this.supertypes
+        .mapNotNull { it.classifier as? KClass<*> }
+        .find { it.annotations.any { a -> a.annotationClass == annotation } }
 
     if (targetInterface == null) {
         log.error("Is not bean. target=${this.simpleName}")
-        return this.simpleName
+        return this.simpleName!!
     }
 
-    return (targetInterface.type as Class<*>).simpleName
+    return targetInterface.simpleName!!
+}
+
+fun KClass<*>.isInterface(): Boolean {
+    return this.java.isInterface
+}
+
+fun KClass<*>.isEnum(): Boolean {
+    return this.java.isEnum
 }
