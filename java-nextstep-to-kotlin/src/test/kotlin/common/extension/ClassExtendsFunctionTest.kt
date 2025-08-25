784d488e51864b6a6a82a608a158c1afefa7f073
kkkkkksssssaaaa <kofc312@naver.com>
2025-08-26 00:00:26 +0900

    test(nextstep-to-kotlin): add KClass.isBean<*>(param) test

--- java-nextstep-to-kotlin/src/test/kotlin/common/extension/ClassExtendsFunctionTest.kt
+++ java-nextstep-to-kotlin/src/test/kotlin/common/extension/ClassExtendsFunctionTest.kt
@@ -5,6 +5,7 @@ import common.factory.models.annotations.Controller
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -14,37 +15,89 @@ class ClassExtendsFunctionTest {
     internal inner class IsBeanTest {
         @Test
         fun `Component 어노테이션이 선언된 클래스는 isBean=true 이다`() {
-            @Component class HasComponent
+            @Component class HasAnnotation
 
             assertTrue {
-                HasComponent::class.isBean()
+                HasAnnotation::class.isBean()
             }
         }
 
         @Test
         fun `Controller 어노테이션이 선언된 클래스는 isBean=true 이다`() {
-            @Controller class HasComponent
+            @Controller class HasAnnotation
 
             assertTrue {
-                HasComponent::class.isBean()
+                HasAnnotation::class.isBean()
             }
         }
 
         @Test
         fun `stereotype 에 해당하지 않는 어노테이션이 선언된 클래스는 isBean=false 이다`() {
-            @NotStereotypeAnnotation class HasComponent
+            @NotStereotypeAnnotation class HasNotAnnotation
 
             assertFalse {
-                HasComponent::class.isBean()
+                HasNotAnnotation::class.isBean()
             }
         }
 
         @Test
         fun `아무런 어노테이션이 선언 되지 않은 클래스는 isBean=false 이다`() {
-            class HasComponent
+            class HasNotAnnotation
 
             assertFalse {
-                HasComponent::class.isBean()
+                HasNotAnnotation::class.isBean()
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("KClass.isBean(param)")
+    internal inner class IsBeanWithParamTest {
+        @Nested
+        @DisplayName("stereotype 에 해당하는 어노테이션이 인자로 주어질 때")
+        internal inner class IsBeanWithStereotypeParamTest {
+            @Test
+            fun `대상 클래스에 인자와 동일한 어노테이션이 선언된 경우 isBean=true 이다`() {
+                @Component class HasAnnotation
+
+                assertTrue {
+                    HasAnnotation::class.isBean<Annotation>(Component::class)
+                }
+            }
+
+            @Test
+            fun `대상 클래스에 인자와 다른 어노테이션이 선언된 경우 isBean=false 이다`() {
+                @Component class HasAnnotation
+
+                assertFalse {
+                    HasAnnotation::class.isBean<Annotation>(Controller::class)
+                }
+            }
+
+            @Test
+            fun `대상 클래스에 stereotype 어노테이션이 선언 되지 않은 경우 isBean=false 이다`() {
+                class HasNotAnnotation
+
+                assertFalse {
+                    HasNotAnnotation::class.isBean<Annotation>(Component::class)
+                }
+
+                assertFalse {
+                    HasNotAnnotation::class.isBean<Annotation>(Controller::class)
+                }
+            }
+        }
+
+        @Nested
+        @DisplayName("stereotype 에 해당하지 않는 어노테이션이 인자로 주어질 때")
+        internal inner class IsBeanWithNotStereotypeParamTest {
+            @Test
+            fun `IllegalArgumentException 을 던진다`() {
+                @Component class HasAnnotation
+
+                assertThrows<IllegalArgumentException> {
+                    HasAnnotation::class.isBean<Annotation>(NotStereotypeAnnotation::class)
+                }
             }
         }
     }
